c1206f734a529fb85d40be6924f45b35e3eeb435
Jayoda Bimsara <jayodadayarathna@gmail.com>
2026-06-10 00:02:01 +0530

    Add Student Input function

--- src/StudentGradeManagementSystem.java
+++ src/StudentGradeManagementSystem.java
@@ -21,3 +21,75 @@ class Student {
         System.out.println("ID: " + studentId + ", Name: " + studentName + ", Marks: " + marks);
     }
 }
+
+public class StudentGradeManagementSystem {
+
+    private static ArrayList<Student> students = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int choice;
+
+        do {
+            System.out.println("===== Student Grade Management System =====");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Students");
+            System.out.println("3. Search Student by ID");
+            System.out.println("4. Calculate Average Marks");
+            System.out.println("5. Exit");
+            System.out.print("Enter Choice: ");
+
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    addStudent();
+                    break;
+
+                case 2:
+                    
+                    break;
+
+                case 3:
+                    
+                    break;
+
+                case 4:
+                  
+                    break;
+
+                case 5:
+                    System.out.println("Thank You!");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice!");
+            }
+
+        } while (choice != 5);
+
+        sc.close();
+    }
+
+   
+    private static void addStudent() {
+        System.out.print("Enter Student ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Marks: ");
+        double marks = sc.nextDouble();
+        sc.nextLine();
+
+        students.add(new Student(id, name, marks));
+
+        System.out.println("Student Added Successfully!");
+    }
+
+    
+   
+}
